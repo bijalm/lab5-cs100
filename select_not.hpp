31fa40e74c77fdcc6fd72a15d643b9e5e89e7cd3
Keeevini <kevin.ni2000@gmail.com>
2021-02-10 17:04:51 -0800

    Fixed the print_selection

---
+++ select_not.hpp
@@ -0,0 +1,20 @@
+#ifndef __SELECT_NOT_HPP__
+#define __SELECT_NOT_HPP__
+
+class Select_Not : public Select
+{
+protected:
+	Select* base;
+public:
+	Select_Not(Select* base)
+	{
+		this->base = base;
+	}
+	
+	bool select(const Spreadsheet* sheet, int row) const
+	{
+		return !(this->base->select(sheet, row));
+	}
+};
+
+#endif //__SELECT_NOT_HPP__
